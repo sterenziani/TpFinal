df2cc8cd94a2d0bf94e7c25f16d94174b8b1446b
sterenziani <sterenziani@itba.edu.ar>
2016-10-29 20:39:25 -0300

    Agregué protopipos para InvalidMove y HayJugada

--- ByTBack.h
+++ ByTBack.h
@@ -3,3 +3,9 @@
 Ej.
   int isGameFinished(int true);
 */
+
+//Revisa si las coordenadas son un movimiento válido
+int InvalidMove(char tablero[N][N], int Fo, int Co, int Ff, int Cf)
+ 
+//Revisa si existe otro movimiento posible
+int HayJugada(char board[N][N])
